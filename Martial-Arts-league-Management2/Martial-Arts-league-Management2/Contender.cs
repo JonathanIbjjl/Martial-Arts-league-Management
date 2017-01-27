d9a80311179156ca3bd28cca511b2f26674ceaf3
Jonathan435 <jonathan88@012.net.il>
2017-01-27 20:47:33 +0200

    Second Commit

---
+++ Martial-Arts-league-Management2/Martial-Arts-league-Management2/Contender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contenders
+{
+    class Contender
+    {
+        public string Name { get; set; }
+        public string Fname { get; set; }
+        public string ID { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DateOfBirth { get; set; } // its not DateTime Type because format is not validated inside google docs
+        public int Age { get; set; }
+        public int AgeCategory { get; set; }
+        public bool IsMale { get; set; }
+        public int Weight { get; set; }
+        public int WeightCategory { get; set; }
+        public int Belt { get; set; }
+        public string AcademyName { get; set; }
+        public string CoachName { get; set; }
+        public string CoachPhone { get; set; }
+        public bool IsAllowedWeightGradeAbove { get; set; }
+        public bool IsAllowedAgeGradeAbove { get; set; }
+        public bool IsAllowedBeltGradeAbove { get; set; }
+        public bool IsChild { get; set; }
+    }
+}
